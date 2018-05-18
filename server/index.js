37066ffa003e9c974b9f1990a9b12a5c36035c50
Nathan <n.hatz@yahoo.com>
2018-05-18 14:25:21 -0500

    Readme Changes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 // As far as structure goes, our server folder should be in the root directory
 // index.js should be inside server
 // and we should run nodemon from the root by running nodemon server/index.js or nodemon server/
-// get used to this because when we get into databases it'll important that we start the server from the root directory.
 const express = require("express");
 const { json } = require("body-parser");
 const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
-
 // App/Server Declaration
 const app = express();
 // Make sure to start your node server (nodemon) first and your react server second.
@@ -20,7 +18,7 @@ app.use(cors());
 // Controller Methods
 const { getData } = require(`${__dirname}/controllers/weather_controller`);
 
-// Get initial State
+// Get initial initial data
 app.get("/data/weather/:search", getData);
 // ----^ CB function is not working, logging as an anon obj----
 
